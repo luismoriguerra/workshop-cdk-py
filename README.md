3dc2dc4757fbccf5468f64bce26cea07506af576
Luis Mori Guerra <luismorith@gmail.com>
2022-12-30 09:25:52 -0500

    feat: adding fargate example
    Signed-off-by: Luis Mori Guerra <luismorith@gmail.com>

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_apigateway/README.html
 https://realpython.com/
 
 # workshop-cdk-py
+https://mauricius.dev/deploy-a-scheduled-workload-on-fargate-using-aws-cdk/
+https://blog.devops.dev/how-to-deploy-fargate-service-serverless-using-aws-cdk-6ffce384b1ae
+https://michalisdaniilakis.com/posts/deploying-to-ecs-fargate-using-the-aws-cdk/
--- cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop_stack.py
@@ -3,6 +3,9 @@ from aws_cdk import (
     Stack,
     aws_lambda as _lambda,
     aws_apigateway as apigw,
+    aws_ec2 as ec2,
+    aws_ecs as ecs,
+    aws_ecs_patterns as ecs_patterns,
 )
 
 from cdk_dynamo_table_view import TableViewer
@@ -13,26 +16,44 @@ class CdkWorkshopStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        
+        
+        ## Fargate example ####
 
-        my_lambda = _lambda.Function(
-            self, 'lgpy-lambda',
-            runtime=_lambda.Runtime.PYTHON_3_7,
-            code=_lambda.Code.from_asset('lambda'),
-            handler='hello.handler',
-        )
+        # create vpc
+        self.vpc = ec2.Vpc(self, "lgpy-vpc", max_azs=2)
+        self.cluster = ecs.Cluster(self, "lgpy-cluster", vpc=self.vpc)
 
-        hello_with_counter = HitCounter(
-            self, 'HelloHitCounter',
-            downstreamFn=my_lambda,
+        # initiate fargate service
+        self.fargate_service = ecs_patterns.ApplicationLoadBalancedFargateService(
+            self, "lgpy-fargate-service",
+            cluster=self.cluster,
+            task_image_options={
+                "image": ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample"),
+            }
         )
+        
+        ########################################
 
-        apigw.LambdaRestApi(
-            self, 'LGEndpoint',
-            handler=hello_with_counter.handler,
-        )
+        # my_lambda = _lambda.Function(
+        #     self, 'lgpy-lambda',
+        #     runtime=_lambda.Runtime.PYTHON_3_7,
+        #     code=_lambda.Code.from_asset('lambda'),
+        #     handler='hello.handler',
+        # )
 
-        TableViewer(
-            self, 'LGViewHitCounter',
-            title='Hello Hits',
-            table=hello_with_counter.table,
-        )
+        # hello_with_counter = HitCounter(
+        #     self, 'HelloHitCounter',
+        #     downstreamFn=my_lambda,
+        # )
+
+        # apigw.LambdaRestApi(
+        #     self, 'LGEndpoint',
+        #     handler=hello_with_counter.handler,
+        # )
+
+        # TableViewer(
+        #     self, 'LGViewHitCounter',
+        #     title='Hello Hits',
+        #     table=hello_with_counter.table,
+        # )
